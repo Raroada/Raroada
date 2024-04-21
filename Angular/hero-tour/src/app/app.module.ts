9469a6205af38d825a269869f6347818004520a3
Dakota <dlongdo2@gmail.com>
2024-04-21 18:42:01 -0500

    Step 5 complete

--- Angular/hero-tour/src/app/app.module.ts
+++ Angular/hero-tour/src/app/app.module.ts
@@ -7,19 +7,23 @@ import { FormsModule } from '@angular/forms';
 import { NgFor } from '@angular/common';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    NgFor
+    NgFor,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular/hero-tour/src/app/heroes/heroes.component.ts
+++ Angular/hero-tour/src/app/heroes/heroes.component.ts
@@ -10,17 +10,11 @@ import { MessageService } from '../messages.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = []
-  selectedHero?: Hero;
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.get_heroes()
-  }
-
-  onselect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   get_heroes(): void {
